0cfe0cc98761e86260918912da0d44b672992ed9
Kevin Edwards <kedwards@kevinedwards.ca>
2019-10-13 10:26:18 -0600

    Updates

---
+++ precache-manifest.b7db06d5a0ebe3727b688db3e0df647e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "024774ed0e3098f46e32db4b5723fb29",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "04d540fc50b120c77868",
+    "url": "/monsters-rolodex/static/css/main.0c6f5ee8.chunk.css"
+  },
+  {
+    "revision": "ca903edc950d52328219",
+    "url": "/monsters-rolodex/static/js/2.e124cbd2.chunk.js"
+  },
+  {
+    "revision": "04d540fc50b120c77868",
+    "url": "/monsters-rolodex/static/js/main.cdb24838.chunk.js"
+  },
+  {
+    "revision": "d71975295fe45a6cbab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.4686dfee.js"
+  }
+]);
